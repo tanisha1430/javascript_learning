5690c260b5ccf1aac22505618735b871c623b737
tanisha1430 <sodhitanisha3005@gmail.com>
2026-09-22 18:14:16 +0530

    datatypes

---
+++ 01_baiscs/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //uses newer version of javascript and it will throw error if we use any variable without declaring it
+console.log("tanisha"); console.log(2+2)
+
+//  number=>2 to power 53
+// string=>sequence of characters
+// boolean=>true or false
+// null>it is an empty value and it is an object
+// undefined=>it is a variable which is declared but not assigned any value to it
+// object=>it is a collection of key value pairs
+// symbol=>it is a unique and immutable data type
+
+console.log(typeof "hitesh")
+console.log(typeof null) // this is not null it is an object because of a bug in javascript which is not fixed yet
+console.log(typeof undefined) // this is undefined because we have not assigned any value to it
